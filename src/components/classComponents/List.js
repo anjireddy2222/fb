aecf17bb778449c91fdd76e660fa52c6175aa9d2
anji-sbd <anjir@sbdautomotive.com>
2021-08-31 19:04:14 +0530

    array sort, filter methods

--- src/components/classComponents/List.js
+++ src/components/classComponents/List.js
@@ -5,13 +5,44 @@ import Child from './Child'
 class List extends React.Component{
     
     constructor(props) {
-        
         super(props)
         this.state = {
             name: "",
             latitude: 0,
             longitude: 0,
-            email:""
+            email:"",
+            employess: [
+                {
+                    empid: 15,
+                    name: "Anji",
+                    city: "Hyderabad",
+                    salary: 15
+                },
+                {
+                    empid: 18,
+                    name: "Pragna",
+                    city: "Bangalore",
+                    salary: 20
+                },
+                {
+                    empid: 9,
+                    name: "User 3",
+                    city: "Chennai",
+                    salary: 25
+                },
+                {
+                    empid: 2,
+                    name: "User 4",
+                    city: "Hyderabad",
+                    salary: 20
+                }
+            ],
+            numbers:[
+                1, 2, 4, 6, 80, 39, 524, 64, 58
+            ],
+            uniNUmbers:[
+                1, 3, 5, 4, 3, 1, 20, 20, 25, 15
+            ]
         }
     }
 
@@ -38,6 +69,48 @@ class List extends React.Component{
         console.log("Page will be removed from the screen")
     }
 
+    handleSort(){
+        var tmpData = [...this.state.employess];
+        tmpData.sort( (emp, prevEmp) =>  prevEmp.empid - emp.empid )
+        this.setState({
+            employess: tmpData
+        })
+    }
+
+    handleAscSort(){
+        var tempData = [...this.state.numbers]
+        tempData.sort( (number, prevNum) => number - prevNum  )
+        this.setState({numbers: tempData})
+    }
+
+    handleDscSort(){
+        var tempData = [...this.state.numbers]; // ... is spread operator
+        tempData.sort( (number, prevNum) => prevNum - number  )
+        this.setState({numbers: tempData})
+    }
+
+    removeDuplicates(){
+        var tempData = [...this.state.uniNUmbers]
+        var newData = []
+        tempData.map( number => {
+            if( newData.includes(number) == false ){
+                newData.push(number)
+            }
+        })
+        this.setState( {uniNUmbers: newData} )
+    }
+
+
+    updateSalaryForHyd(){
+        var tempData = [...this.state.employess]
+        tempData.map( emp => {
+            if(emp.city == "Hyderabad"){
+                emp.salary = emp.salary + 10
+            }
+            return emp;
+        })
+        this.setState({employess: tempData})
+    }
 
 
 
@@ -54,7 +127,54 @@ class List extends React.Component{
                 <Child />
                 <br/>
                 <button className="btn btn-primary" onClick={e => this.handleNameChange() }>Change user name</button>
-
+                <table>
+                    <thead>
+                        <tr>
+                            <td onClick={ e => this.handleSort() }>
+                                Emp id
+                            </td>
+                            <td>Name</td>
+                            <td>City</td>
+                            <td onClick={ e => this.updateSalaryForHyd() } >Salary</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.employess.map( emp => (
+                                <tr>
+                                    <td>{emp.empid}</td>
+                                    <td>{emp.name}</td>
+                                    <td>{emp.city}</td>
+                                    <td>{emp.salary}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+                <br/>
+                <div>
+                    <button className="btn btn-success" onClick={ e => this.handleAscSort() }> Asc order </button> &nbsp; &nbsp;
+                    <button className="btn btn-danger" onClick={ e => this.handleDscSort() } >Dsc Order</button>
+                    {
+                        this.state.numbers.map( number => (
+                            <div>
+                                {number}
+                            </div>
+                        ))
+                    }
+                </div>
+                <div>
+                    <button className="btn btn-primary" onClick={ e => this.removeDuplicates() }>Remove duplicates</button>
+                    <div>
+                        {
+                            this.state.uniNUmbers.map( num => (
+                                <div>
+                                    {num}
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
         )
     }
